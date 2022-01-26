281f8417c422e078d7dc7da65d30ca5aeda63aa1
AlexeyKonstantinov78 <74707207+AlexeyKonstantinov78@users.noreply.github.com>
2022-01-26 09:12:35 +0300

    upgrade script burger menu and add section booking

--- src/js/menu-burger.js
+++ src/js/menu-burger.js
@@ -8,7 +8,14 @@ const toogle = () => {
     menuBurgerNav.classList.toggle('activ');
 };
 
-menuBurgerBtn.addEventListener('click', () => {
-    
+menuBurgerBtn.addEventListener('click', () => {    
     toogle();
+});
+
+menuBurgerNav.addEventListener('click', (event) => {
+    let target = event.target;
+
+    if (target.classList.contains('menu-burger__link')) {
+        toogle();
+    }
 });
